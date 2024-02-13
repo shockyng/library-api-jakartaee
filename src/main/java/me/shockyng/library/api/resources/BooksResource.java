516afd1e667252211810978fd864d84af4074403
Luan Santos <shockyng@icloud.com>
2024-02-13 15:21:21 +0000

    add books resource

--- src/main/java/me/shockyng/library/api/resources/BooksResource.java
+++ src/main/java/me/shockyng/library/api/resources/BooksResource.java
@@ -6,6 +6,8 @@ import jakarta.ws.rs.core.MediaType;
 import me.shockyng.library.api.data.dtos.BookDTO;
 import me.shockyng.library.api.data.dtos.BookWithAuthorsDTO;
 import me.shockyng.library.api.services.BooksService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,40 +17,48 @@ import java.util.UUID;
 @Consumes(value = MediaType.APPLICATION_JSON)
 public class BooksResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BooksResource.class);
+
     @Inject
     private BooksService service;
 
     @POST
     public BookDTO createBook(BookDTO bookDTO) {
+        LOGGER.info("Create book request received with payload: {}", bookDTO);
         return service.createBook(bookDTO);
     }
 
     @GET
     public List<BookDTO> getAllBooks() {
+        LOGGER.info("Get all books request received");
         return service.getAllBooks();
     }
 
     @GET
     @Path("/authors")
     public List<BookWithAuthorsDTO> getAllBooksWithAuthors() {
+        LOGGER.info("Get all books with authors request received");
         return service.getAllBooksWithAuthors();
     }
 
     @GET
     @Path("/{id}")
     public BookDTO getBookById(@PathParam("id") Long id) {
+        LOGGER.info("Get book request received with id: {}", id);
         return service.getBookById(id);
     }
 
     @PUT
     @Path("/{id}")
     public BookDTO updateBook(@PathParam("id") Long id, BookDTO bookDTO) {
+        LOGGER.info("Update book request received with id {} and payload: {}", id, bookDTO);
         return service.updateBook(id, bookDTO);
     }
 
     @DELETE
     @Path("/{id}")
     public void deleteBook(@PathParam("id") Long id) {
+        LOGGER.info("Delete book request received with id: {}", id);
         service.deleteBook(id);
     }
 }
