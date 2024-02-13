d3117d6eae3e4057b99dd45562b45c61768e0f0d
Luan Santos <shockyng@icloud.com>
2024-02-13 15:20:17 +0000

    add log on base dao

--- src/main/java/me/shockyng/library/api/daos/BaseDAO.java
+++ src/main/java/me/shockyng/library/api/daos/BaseDAO.java
@@ -5,11 +5,16 @@ import jakarta.persistence.Persistence;
 import jakarta.persistence.PersistenceException;
 import jakarta.persistence.RollbackException;
 import me.shockyng.library.api.exceptions.NoDataAtTheDataBaseException;
+import me.shockyng.library.api.services.PeopleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Objects;
 
 public abstract class BaseDAO<E, ID> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PeopleService.class);
 
     protected final EntityManager entityManager;
     protected Class<E> entityClass;
@@ -20,12 +25,14 @@ public abstract class BaseDAO<E, ID> {
     }
 
     public E findOneById(ID id) {
+        LOGGER.info("Fetch at the database a {} with id: {}", entityClass.getName(), id);
         E entity = entityManager.find(entityClass, id);
         verifyIfEntityNull(entity, id);
         return entity;
     }
 
     public List<E> findAll() {
+        LOGGER.info("Fetch at the database all {}", entityClass.getName());
         return entityManager
                 .createQuery(String.format("select e from %s e", entityClass.getName()), entityClass)
                 .getResultList();
@@ -33,6 +40,7 @@ public abstract class BaseDAO<E, ID> {
 
     public E save(E entity) {
         beginTransaction();
+        LOGGER.info("Save/update {} at the database", entityClass.getName());
         entityManager.merge(entity);
         commitTransaction();
         return entity;
@@ -42,12 +50,14 @@ public abstract class BaseDAO<E, ID> {
         E entity = findOneById(id);
         verifyIfEntityNull(entity, id);
         beginTransaction();
+        LOGGER.info("Delete {} at the database with id: {}", entityClass.getName(), id);
         entityManager.remove(entity);
         commitTransaction();
     }
 
     private void beginTransaction() {
         try {
+            LOGGER.info("Begin transaction");
             entityManager.getTransaction().begin();
         } catch (IllegalStateException e) {
             rollBackTransaction();
@@ -56,6 +66,7 @@ public abstract class BaseDAO<E, ID> {
 
     private void commitTransaction() {
         try {
+            LOGGER.info("Commit transaction");
             entityManager.getTransaction().commit();
         } catch (IllegalStateException | RollbackException e) {
             rollBackTransaction();
@@ -64,6 +75,7 @@ public abstract class BaseDAO<E, ID> {
 
     private void rollBackTransaction() {
         try {
+            LOGGER.info("Rollback transaction");
             entityManager.getTransaction().rollback();
         } catch (IllegalStateException | PersistenceException e) {
             e.printStackTrace();
@@ -71,6 +83,7 @@ public abstract class BaseDAO<E, ID> {
     }
 
     private void verifyIfEntityNull(E entity, ID id) {
+        LOGGER.info("Verifying if {} with id: {} is null", entityClass.getName(), id);
         if (Objects.isNull(entity)) throw new NoDataAtTheDataBaseException(entityClass, id);
     }
 
